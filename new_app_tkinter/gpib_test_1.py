6af33a2bf39aa6e4a713558af601d508ccf96990
Mario Schwabe Filho <mario.schwabe@se.com>
2020-12-14 13:57:28 +0100

    14.12.2020

--- new_app_tkinter/gpib_test_1.py
+++
@@ -1,8 +0,0 @@
-import pyvisa
-rm = pyvisa.ResourceManager()
-print(rm.list_resources())
-# import pyvisa
-# import ctypes
-# ctypes.windll.LoadLibrary(r"C:\Windows\System32\gpib-32.dll")
-# rm = pyvisa.ResourceManager('@py')
-# print(rm.list_resources())
