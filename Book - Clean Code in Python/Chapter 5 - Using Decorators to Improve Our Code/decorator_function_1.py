3cb1c9b4c61c98d71d116b32e03e1e477369da07
marioschwabefilho92 <56764467+marioschwabefilho92@users.noreply.github.com>
2021-01-08 17:01:04 +0100

    08.01.2021

--- Book - Clean Code in Python/Chapter 5 - Using Decorators to Improve Our Code/decorator_function_1.py
+++ Book - Clean Code in Python/Chapter 5 - Using Decorators to Improve Our Code/decorator_function_1.py
@@ -1,51 +1,23 @@
-# class ControlledExecption(Exception):
-#     """A generic exception on the program's domain."""
+def uppercase(func):
+    def wrapper():
+        original_result = func()
+        modified_result = original_result.upper()
+        return modified_result
+    return wrapper
 
-# def retry(operation):
-#     @wraps(operation)
-#     def wrapped(*args, **kwargs):
-#         last_raised = None
-#         RETRIES_LIMIT = 3
-#         for _ in range(RETRIES_LIMIT):
-#             try:
-#                 return operation(*args, **kwargs)
-#             except ControlledExecption as e:
-#                 logger.info("Retrying %s", operation.__qualname__)
-#                 last_raised = e
-#         raise last_raised
-#     return wrapped
+# Print Hello in UPPERCASE
+@uppercase
+def greet():
+    return 'Hello!'
 
-# @retry
-# def run_operation(task):
-#     """"Run a particular task, simulating some failures on its execution."""
-#     return task.run()
+print(greet())
+print(type(greet()))
+print(id(greet()))
 
-def star(func):
-    def inner(*args, **kwargs):
-        print("*" * 30)
-        func(*args, **kwargs)
-        print("*" * 30)
-    return inner
-
-
-def percent(func):
-    def inner(*args, **kwargs):
-        print("%" * 30)
-        func(*args, **kwargs)
-        print("%" * 30)
-    return inner
-
-@percent
-@star
-def printer(msg):
-    print(msg)
-printer("Hello")
-print("")
-print("")
-print("")
-
-def printer(msg):
-    print(msg)
-printer = star(percent(printer))
-
-printer("Hello")
+# Print Hello
+def greet():
+    return 'Hello!'
+    
+print(greet())
+print(type(greet()))
+print(id(greet()))
